ceed13bca08938b731aa978d31f439d348cf4e7c
bassemhossam <31673179+bassemhossam@users.noreply.github.com>
2019-05-05 17:45:23 +0200

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -11,14 +11,15 @@ from pynput.keyboard import Key, Controller
 from cv_code import Gesture_detector
 from prompt import PromptWindow
 
-detector_obj = Gesture_detector()
+detector_obj = Gesture_detector() 
 
 
 class Thread(QThread):
+    #custom thread class that is used to update the camera frames in the gui
     changeframe = pyqtSignal(np.ndarray)
 
     def run(self):
         while True:
-            ret, frame = detector_obj.frame_with_ROI()
+            ret, frame = detector_obj.frame_with_ROI() # get the current camera frame + Region of interest
             if ret:
-                self.changeframe.emit(frame)
+                self.changeframe.emit(frame) # emit signal to send the frame to the view_image function
