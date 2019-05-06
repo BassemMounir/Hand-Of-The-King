1fd4a978735fd035dbaf57a6f432897ce80828c1
BassemMounir <bassem.mounir.elias@gmail.com>
2019-05-06 09:26:59 +0200

    addiny some helper function
    adding average_and_join_points and remove_noisy_defects function

--- helper.py
+++ helper.py
@@ -23,3 +23,41 @@ class HelperFunctions():
                 return angle
             angle = math.acos(eq) * (180 / math.pi)  # cosine theorem
         return angle
+    
+    def average_and_join_points(self, points, min_distance):  # for clustering defects near to each other
+        final_points = []
+        avg_array = []
+        if points is not None:
+            for i in range(1, points.shape[0]):  # calculate the angle
+                x1 = points[i - 1][0][0]
+                y1 = points[i - 1][0][1]
+                x2 = points[i][0][0]
+                y2 = points[i][0][1]
+                point1 = (x1, y1)
+                point2 = (x2, y2)
+                if self.distance(point1, point2) < min_distance:
+                    if point1 not in avg_array:
+                        avg_array.append(point1)
+                    if point2 not in avg_array:
+                        avg_array.append(point2)
+                else:
+                    if len(avg_array) > 0:
+                        final_points.append(self.get_avg_point(avg_array))
+                        avg_array.clear()
+                    else:
+                        if point1 not in avg_array:
+                            final_points.append(point1)
+        return final_points
+
+    def remove_noisy_defects(self, res, defects):  # for removing defects not on the hand
+        new_defects = []
+        for j in range(0, len(defects)):
+            s1, e1, f1, d1 = defects[j][0]
+            start1 = tuple(res[s1][0])
+            end1 = tuple(res[e1][0])
+            far1 = tuple(res[f1][0])
+            if self.distance(start1, end1) > 10:
+                new_defects.append((start1, end1, far1))
+        return new_defects
+
+
